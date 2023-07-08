cf2d5f80a3fcd20c1e79f2445082f2524ff6a996
Oerd Bej <oerdbej@gmail.com>
2023-07-08 15:53:54 +0200

    middle section description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,34 @@ import React from 'react';
 import { HiArrowNarrowRight } from 'react-icons/hi';
 
 const Home = () => {
-    return <div className='bg-[#414756] w-full h-screen'>Home</div>;
+    return (
+        <div className='w-full h-screen bg-[#0a192f]' name='name'>
+            {/* Container */}
+            <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full border-2 border-red-500'>
+                <p className='text-pink-500'>Hi my name is</p>
+                <h1 className='text-4xl sm:text-7xl font-bold-text text-[#ccd6f6]'>
+                    Oerd Bej
+                </h1>
+                <h2 className='text-4xl sm:text-7xl font-bold-text text-[#8892b0] border-2 border-red-500'>
+                    Full stack
+                </h2>
+                <p className='text-[#8892b0] py-4 max-w-[700px]'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Tempora hic nisi assumenda minima autem. Consectetur soluta
+                    illum delectus laborum mollitia harum error expedita.
+                </p>
+                {/* button */}
+                <div>
+                    <button className='text-white border-2 px-6 py-3 my-2 flex-items-center hover:bg-pink-500 hover:border-pink-300 border-2 border-red-500 '>
+                        View Work
+                        <span>
+                            <HiArrowNarrowRight className='ml-7 ' />
+                        </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
             </ul>
 
             {/* social */}
-            <div className='flex fixed flex-col top-[35%] left-0'>
+            <div className=' hidden lg:flex   flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
                         <a
