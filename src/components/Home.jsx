78e414f363444f3fd21eda0796be27fe258eac7f
Oerd Bej <oerdbej@gmail.com>
2023-07-08 12:30:50 +0200

    social media link icons with hover

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+
+const Home = () => {
+    return <div className='bg-[#414756] w-full h-screen'>Home</div>;
+};
+
+export default Home;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import logo from '../assets/oerdbej.png';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import {
+    FaBars,
+    FaTimes,
+    FaLinkedin,
+    FaGithub,
+    FaFacebook,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -27,7 +35,7 @@ const Navbar = () => {
 
             {/* hamburger: medium hide icons*/}
             <div onClick={handleClick} className='md:hidden z-10'>
-                <FaBars />
+                {!nav ? <FaBars /> : <FaTimes />}
             </div>
 
             {/* mobile */}
@@ -45,7 +53,38 @@ const Navbar = () => {
             </ul>
 
             {/* social */}
-            <div className='hidden'></div>
+            <div className='flex fixed flex-col top-[35%] left-0'>
+                <ul>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'>
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Linkedin <FaLinkedin size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#3333]'>
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Github <FaGithub size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg'>
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Email <HiOutlineMail size={30} />
+                        </a>
+                    </li>
+                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+                        <a
+                            className='flex justify-between items-center w-full text-gray-300'
+                            href='/'>
+                            Resume <BsFillPersonLinesFill size={30} />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
