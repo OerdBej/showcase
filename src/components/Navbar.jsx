2a4062840b54479668100e6d6e4ad1ee2a6f1921
Oerd Bej <oerdbej@gmail.com>
2023-07-08 12:05:17 +0200

    hamburger functionality for toggling it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import logo from '../assets/oerdbej.png';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 
 const Navbar = () => {
+    // toggle
+    const [nav, setNav] = useState(false);
+
+    const handleClick = () => {
+        setNav(!nav);
+    };
     return (
         <div className='fixed w-full h-[80px] flex justify-between text-gray-300 items-center px-4 bg-[#414756] border-2 border-red-500'>
             <div className='border-2 border-red-500'>
@@ -19,12 +26,17 @@ const Navbar = () => {
             </ul>
 
             {/* hamburger: medium hide icons*/}
-            <div className='md:hidden z-10'>
+            <div onClick={handleClick} className='md:hidden z-10'>
                 <FaBars />
             </div>
 
             {/* mobile */}
-            <ul className='absolute top-0 left-0 w-full h-screen bg-[#414756] flex flex-col justify-center items-center'>
+            <ul
+                className={
+                    !nav
+                        ? 'hidden'
+                        : 'absolute top-0 left-0 w-full h-screen bg-[#414756] flex flex-col justify-center items-center '
+                }>
                 <li className='p-6 text-4xl'>Home</li>
                 <li className='p-6 text-4xl'>About</li>
                 <li className='p-6 text-4xl'>Experience</li>
