511d56c844e6532d3c5a510b5078a7098a8f0662
Oerd Bej <oerdbej@gmail.com>
2023-07-08 11:43:16 +0200

    updating the navbar for medium size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,30 +8,32 @@ const Navbar = () => {
             <div className='border-2 border-red-500'>
                 <img src={logo} alt='oerd bej' style={{ width: '140px' }} />
             </div>
+
             {/* list items menu */}
-            <div>
-                <ul className='flex'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Work</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-            {/* Hamburger */}
-            <div className='hidden'>
+            <ul className='hidden md:flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Work</li>
+                <li>Contact</li>
+            </ul>
+
+            {/* hamburger: medium hide icons*/}
+            <div className='md:hidden z-10'>
                 <FaBars />
             </div>
-            {/* logo */}
-            <div>
-                <ul className='hidden'>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Experience</li>
-                    <li>Work</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
+
+            {/* mobile */}
+            <ul className='absolute top-0 left-0 w-full h-screen bg-[#414756] flex flex-col justify-center items-center'>
+                <li className='p-6 text-4xl'>Home</li>
+                <li className='p-6 text-4xl'>About</li>
+                <li className='p-6 text-4xl'>Experience</li>
+                <li className='p-6 text-4xl'> Work</li>
+                <li className='p-6 text-4xl'>Contact</li>
+            </ul>
+
+            {/* social */}
+            <div className='hidden'></div>
         </div>
     );
 };
