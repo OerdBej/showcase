dd862e1f9b181d53a30200525f7bf2594564b6b7
Oerd Bej <oerdbej@gmail.com>
2023-07-11 21:51:38 +0200

    grid section for the skills boiler plate ready - need to replacate all skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,11 +9,27 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
     return (
-        <div name='skills'>
-            <div className=''>
+        <div name='skills' className='bg-[#0a192f] text-gray-300'>
+            {/* the div container */}
+            <div className='max-w-[1000px] mx-auto px-4 flex flex-col justify-center w-full h-full'>
                 <div>
-                    <p>Experiences</p>
-                    <p>This are some of the stacks that i used</p>
+                    <p className='text-4xl font-bold inline  border-b-4 border-pink-600'>
+                        Experiences
+                    </p>
+                    <p className='py-4'>
+                        This are some of the stacks that i used
+                    </p>
+                </div>
+                {/* div container for all the icons*/}
+                <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                    <div className='shadow-md shadow-pink hover:scale-110 duration-300'>
+                        <img
+                            className='w-20 mx-auto'
+                            src={HTML}
+                            alt='html logo'
+                        />
+                        <p className='my-4'>HTML</p>
+                    </div>
                 </div>
             </div>
         </div>
