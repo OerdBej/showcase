f88c87dbbeb45b2a242b88dbce0518cc9ca256e4
Oerd Bej <oerdbej@gmail.com>
2023-07-13 22:25:03 +0200

    updating the grid layout for the work container

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,14 +6,29 @@ const Work = () => {
             name='work'
             className='bg-[#0a192f] w-full md:h-screen text-gray-300'>
             {/* container for projects*/}
-            <div className='max-w-[1000px] mx-auto p-4'>
-                <p>Work</p>
-                <p>Look at some of my works</p>
-            </div>
+            <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+                <div className='pb-8'>
+                    <p className='text-4xl font-bold inline border-b-4 text-grey-300 border-pink-500'>
+                        Work
+                    </p>
+                    <p className='py-6'>Look at some of my works</p>
+                </div>
 
-            <div>
-                <div>
-                    <button>here</button>
+                {/* grid container */}
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                    <div>
+                        <div>
+                            <span></span>
+                            <div>
+                                <a href='/'>
+                                    <button></button>
+                                </a>
+                                <a href='/'>
+                                    <button></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
