d641a121f5f972b97110c46db0129456f25caa11
Oerd Bej <oerdbej@gmail.com>
2023-07-21 13:31:15 +0200

    fixed the project for the container and some styles

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -17,11 +17,11 @@ const Work = () => {
                 </div>
 
                 {/* grid container */}
-                <div
-                    style={{ backgroundImage: `url(${LoopStudio})` }}
-                    className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                     {/* the shadow section */}
-                    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
+                    <div
+                        style={{ backgroundImage: `url(${LoopStudio})` }}
+                        className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div '>
                         {/* hover */}
                         <div className='opacity-0 group-hover:opacity-100'>
                             <span className='text-2xl font-bold text-white tracking-wider'>
@@ -29,10 +29,14 @@ const Work = () => {
                             </span>
                             <div className='pt-8 text-center'>
                                 <a href='/'>
-                                    <button>Demo</button>
+                                    <button className='text-cetner rounded-lg px-4 py-3 m-2 bg-white text-gray-600 font-bold text-lg'>
+                                        Demo
+                                    </button>
                                 </a>
                                 <a href='/'>
-                                    <button>Code</button>
+                                    <button className='text-cetner rounded-lg px-4 py-3 m-2 bg-white text-gray-600 font-bold text-lg'>
+                                        Code
+                                    </button>
                                 </a>
                             </div>
                         </div>
