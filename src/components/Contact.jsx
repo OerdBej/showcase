8cd00508bdcfdd5747fe8561a9587cfa5e4d69f1
Oerd Bej <oerdbej@gmail.com>
2023-07-29 15:52:46 +0200

    adding the form in the contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,20 +2,36 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            name='contact' className='w-full h-screen bg-[#0a192f] flex
-            justify-center items-center p-4 border-2 border-red-500'>
+        <div
+            name='contact'
+            className='w-full h-screen bg-[#0a192f] flex
+            justify-center items-center p-4 '>
             <form action='' className='flex flex-col max-w-[600px] w-full'>
-                <div className='pb-8 border-2 border-green-400'>
+                <div className='pb-8   '>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-500 text-gray-300'>
                         Contact
                     </p>
-                    <p className='text-gray-300 py-4 border-2 border-yellow-300'>
-                        Contact email
-                    </p>
+                    <p className='text-gray-300 py-4'>Contact email</p>
                 </div>
-                <input type='text' placeholder='Name' name='name' />
-                <input type='text' placeholder='Email' name='email' />
+                {/* input form */}
+                <input
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    className='bg-[#ccd6f6] p-2'
+                />
+                <input
+                    type='text'
+                    placeholder='Email'
+                    name='email'
+                    className='my-4 p-2 bg-[#ccd6f6]'
+                />
+                <textarea
+                    placeholder='afkl;adfkl;hasdf'
+                    name='message'
+                    cols='30'
+                    rows='10'
+                    className='bg-[#ccd6f6] p-2'></textarea>
             </form>
         </div>
     );
